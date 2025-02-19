f0ad16788085a73a44369e0e69592fbf5409e691
morteza <hadafmahdi@gmail.com>
2025-02-19 11:36:24 +0330

    add fonts and its configs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,17 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      shabnam: ['shabnam', 'sans-serif'],
+      shabnamMedium: ['shabnamMedium', 'sans-serif'],
+      shabnamBold: ['shabnamBold', 'sans-serif'],
+      dana: ['dana', 'sans-serif'],
+      danaBold: ['danaBold', 'sans-serif'],
+      danaMedium: ['danaMedium', 'sans-serif'],
+      morabba: ['morabba', 'sans-serif'],
+      morabbaBold: ['morabbaBold', 'sans-serif'],
+      morabbaMedium: ['morabbaMedium', 'sans-serif'],
+    },
     extend: {
       colors: {
         background: "var(--background)",
